fe314fa89be65a3afec6c76ebfbaeb8e34178812
soul <49824752+kayden173800@users.noreply.github.com>
2026-06-17 19:00:20 -0400

    Specify template folder for Flask app

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import sqlite3
 from database import init_db
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 init_db()
 
 
